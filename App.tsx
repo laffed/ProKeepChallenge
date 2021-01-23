37f7d7f70439d9de1daf3910c7904793dbac5ae1
laffed <roarklaffed@gmail.com>
2021-01-23 13:33:28 -0600

    Overmind Provider hooked up

--- App.tsx
+++ App.tsx
@@ -1,23 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import {config} from './overmind';
+import {Provider} from 'overmind-react';
+import {createOvermind} from 'overmind';
+
+const overmind = createOvermind(config, {
+  devtools: '192.168.1.66:3031'
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+  if (isLoadingComplete) {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <Provider value={overmind}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </Provider>
     );
+  } else {
+    return null;
   }
 }
